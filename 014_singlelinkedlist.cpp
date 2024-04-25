f5e537b322b20581aec4ca553272c647a7315219
ahsanhf <ahsanhf@gmail.com>
2024-04-25 17:07:09 +0700

    memeriksa nim jika sudah ada maka kembalikan

--- 014_singlelinkedlist.cpp
+++ 014_singlelinkedlist.cpp
@@ -36,3 +36,13 @@ void addNode() {
 	Node* current = START;
 
 	while ((current != NULL) && (nim >= current->noMhs))
+
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+	previous = current;
+	current = current->next;
+
+}
